a87d24c43676ea99449a4701b9ca6ac5dcdc56cb
Appw <austinpaulpal@gmail.com>
2020-01-25 18:07:57 -0800

    Fixed comments
    They no longer crash! (yay)
    They now also are java-doc and no longer python docstrings lol
    Also now index is using one motor

--- src/main/java/frc/robot/subsystems/Index.java
+++ src/main/java/frc/robot/subsystems/Index.java
@@ -11,34 +11,38 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj.Timer;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
+/**
+* Index and Intake subsystem!
+* @author Appw
+* @version -1.0
+ */
 public class Index extends SubsystemBase {
   private static double indexSpeed = 0.5d; // TODO: move to constants.java
   private static double timeForConsume = 1.0d; // TODO: move to constants.java
-  private WPI_TalonSRX indexMotor0;
-  private WPI_TalonSRX indexMotor1;
-  private WPI_TalonSRX indexMotor2;
+  private WPI_TalonSRX indexMotor;
   private WPI_TalonSRX intakeMotor;
 
+  /**
+  * Constructor for the Index subsystem.
+  */
   public Index() {
-    """Constructor for the Index subsystem, The Intake system is also controlled here.
-    """
-    indexMotor0 = new WPI_TalonSRX(4); // TODO: move to constants.java
-    indexMotor1 = new WPI_TalonSRX(5); // TODO: move to constants.java
-    indexMotor2 = new WPI_TalonSRX(6); // TODO: move to constants.java
-    intakeMotor = new WPI_TalonSRX(7); // TODO: move to constants.java
+    indexMotor = new WPI_TalonSRX(4); // TODO: move to constants.java
+    intakeMotor = new WPI_TalonSRX(5); // TODO: move to constants.java
   }
 
+  /**
+  * Input game piece from the Intake system, one at a time only.
+  */
   public void consume() {
-    """Input game piece from the Intake system, one at a time only.
-    """
   }
 
+  /**
+  * Removes game pieces from the Index system.
+  *
+  * @param reverse If true, the direction of removal and the Intake system are reversed. If not provided, false is assumed.
+  * @param amount Amount of pieces to remove. If not provided, one piece is assumed.
+  */
   public void remove(boolean reverse, int amount) {
-    """Removes game pieces from the Index system.
-
-    :param reverse: If true, the direction of removal and the Intake system are reversed. If not provided, false is assumed.
-    :param amount: Amount of pieces to remove. If not provided, one piece is assumed.
-    """
   }
 
   public void remove(boolean reverse) {
@@ -53,32 +57,36 @@ public class Index extends SubsystemBase {
     remove(false, 1);
   }
 
+  /**
+  * Removes all game pieces from the Index system.
+  * @param reverse If true, the direction of removal and the Intake system are reversed. If not provided, false is assumed.
+  */
   public void removeAll(boolean reverse) {
-    """Removes all game pieces from the Index system.
-    :param reverse: If true, the direction of removal and the Intake system are reversed. If not provided, false is assumed.
-    """
   }
 
   public void removeAll() {
     removeAll(false);
   }
 
+  /**
+  * Stops all movement of the Index system.
+  */
   public void stop() {
-    """Stops all movement of the Index system.
-    """
   }
 
+  /**
+  * Stops all movement of the Index and Intake system.
+  */
   public void stopAll() {
-    """Stops all movement of the Index and Intake system.
-    """
     stop();
   }
 
+  /**
+  * Sets the status of the Intake system.
+  * @param enabled The enabled status of the Intake. If not provided, the status will be toggled.
+  * @param reverse If true, the direction of the Intake will be reversed. If not provided, false is assumed.
+  */
   public void setIntake(boolean enabled, boolean reverse) {
-    """Sets the status of the Intake system.
-    :param enabled: The enabled status of the Intake. If not provided, the status will be toggled.
-    :param reverse: If true, the direction of the Intake will be reversed. If not provided, false is assumed.
-    """
   }
 
   public void setIntake(boolean enabled) {
