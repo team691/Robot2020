624a45c2d109700d24435ecfa4c31c7fcb761c94
nicksstevenson <59856153+nicksstevenson@users.noreply.github.com>
2020-02-01 09:40:39 -0800

    Color Sensor

--- src/main/java/frc/robot/commands/PositionalControl.java
+++ src/main/java/frc/robot/commands/PositionalControl.java
@@ -1,45 +1,23 @@
-/*----------------------------------------------------------------------------*/
-/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
-/*----------------------------------------------------------------------------*/
-
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
-
 import frc.robot.subsystems.*;
 
 public class PositionalControl extends CommandBase {
-  /**
-   * Creates a new PositionalControl.
-   */
-
   private ColorWheel cw = new ColorWheel();
 
-  public PositionalControl() {
-    // Use addRequirements() here to declare subsystem dependencies.
-  }
-
-  // Called when the command is initially scheduled.
-  @Override
-  public void initialize() {
-  }
-
-  // Called every time the scheduler runs while the command is scheduled.
+  public PositionalControl() { }
+  
   @Override
   public void execute() {
     cw.positionalControl();
   }
 
-  // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
     cw.stopColorWheel();
   }
 
-  // Returns true when the command should end.
   @Override
   public boolean isFinished() {
     return false;
--- src/main/java/frc/robot/commands/RotationalControl.java
+++ src/main/java/frc/robot/commands/RotationalControl.java
@@ -1,42 +1,24 @@
-/*----------------------------------------------------------------------------*/
-/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
-/*----------------------------------------------------------------------------*/
-
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.*;
 
 public class RotationalControl extends CommandBase {
-  /**
-   * Creates a new RotationalControl.
-   */
+
   private ColorWheel cw = new ColorWheel();
-  public RotationalControl() {
-    // Use addRequirements() here to declare subsystem dependencies.
-  }
 
-  // Called when the command is initially scheduled.
-  @Override
-  public void initialize() {
-  }
+  public RotationalControl() { }
 
-  // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
     cw.rotationalControl();
   }
 
-  // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
     cw.stopColorWheel();
   }
 
-  // Returns true when the command should end.
   @Override
   public boolean isFinished() {
     return false;
